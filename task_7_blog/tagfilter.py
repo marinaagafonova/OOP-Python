e164c65f6e011f8eb001d1b6808637e59965f396
marinaagafonova <agafonova@cs.vsu.ru>
2019-05-28 11:11:43 +0300

    editing filter

--- task_7_blog/tagfilter.py
+++ task_7_blog/tagfilter.py
@@ -3,12 +3,12 @@ class Filter:
     @staticmethod
     def filter_tags(inputstr):
         inputstr = list(inputstr)
-        styletags = ("b", "i", "h1", "h2", "h3", "h4", "h5", "h6", "tt", "cite", "em", "p", "blockquote", "ol", "li", "ul")
+        styletags = ("b", "i", "h1", "h2", "h3", "h4", "h5", "h6", "tt", "cite", "em", r'font.', r'a.', r'p.', "blockquote", "ol", "li", "ul")
         tag = ""
-        tag_end = True
-        changes = {}  # ключ - индекс открыв тега, значение - индекс закрывающегося тега
+        tag_end = False
+        # changes = {}   ключ - индекс открыв тега, значение - индекс закрывающегося тега
         for i in range(1, len(inputstr)):
-            if tag_end and (inputstr[i - 1] == '<' and inputstr[i] != '/') or inputstr[i - 1] == '/' and inputstr[i] != '>':
+            if (inputstr[i - 1] == '<' and inputstr[i] != '/') or inputstr[i - 1] == '/' and inputstr[i] != '>':
                 tag += inputstr[i]
                 tag_end = False
                 continue
@@ -18,17 +18,17 @@ class Filter:
                 else:
                     tag_end = True
                     if not(tag in styletags):
-                        changes[i - len(tag) - 1] = i
+                        key = i - len(tag) - 1
+                        value = i
+                        if(inputstr[key] == '/' or inputstr[key + 1] == '/'):
+                            if inputstr[key] == '/':
+                                inputstr[key - 1] = ""
+                            else:
+                                inputstr[key + 1] = ""
+                        inputstr[key] = "&lt"
+                        inputstr[value] = "&gt"
+                        # changes[i - len(tag) - 1] = i
                     tag = ""
-        for key, value in changes.items():
-            if(inputstr[key] == '/' or inputstr[key + 1] == '/'):
-                if inputstr[key] == '/':
-                    inputstr[key - 1] = ""
-                else:
-                    inputstr[key + 1] = ""
-            inputstr[key] = "&lt"
-            inputstr[value] = "&gt"
-        return ''.join(inputstr)
 
 
 
--- testing filter/myfilter.py
+++ testing filter/myfilter.py
@@ -6,7 +6,7 @@ class Filter:
         styletags = ("b", "i", "h1", "h2", "h3", "h4", "h5", "h6", "tt", "cite", "em", r'font.', r'a.', r'p.', "blockquote", "ol", "li", "ul")
         tag = ""
         tag_end = False
-        changes = {}  # ключ - индекс открыв тега, значение - индекс закрывающегося тега
+        # changes = {}   ключ - индекс открыв тега, значение - индекс закрывающегося тега
         for i in range(1, len(inputstr)):
             if (inputstr[i - 1] == '<' and inputstr[i] != '/') or inputstr[i - 1] == '/' and inputstr[i] != '>':
                 tag += inputstr[i]
@@ -18,8 +18,18 @@ class Filter:
                 else:
                     tag_end = True
                     if not(tag in styletags):
-                        changes[i - len(tag) - 1] = i
+                        key = i - len(tag) - 1
+                        value = i
+                        if(inputstr[key] == '/' or inputstr[key + 1] == '/'):
+                            if inputstr[key] == '/':
+                                inputstr[key - 1] = ""
+                            else:
+                                inputstr[key + 1] = ""
+                        inputstr[key] = "&lt"
+                        inputstr[value] = "&gt"
+                        # changes[i - len(tag) - 1] = i
                     tag = ""
+        '''
         for key, value in changes.items():
             if(inputstr[key] == '/' or inputstr[key + 1] == '/'):
                 if inputstr[key] == '/':
@@ -28,6 +38,7 @@ class Filter:
                     inputstr[key + 1] = ""
             inputstr[key] = "&lt"
             inputstr[value] = "&gt"
+            '''
         return ''.join(inputstr)
 
 
